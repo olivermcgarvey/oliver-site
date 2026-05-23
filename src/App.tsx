4855804203c6c4d74b52f41a96941a6d9e40f080
olivermcgarvey <olivermcgarvey@gmail.com>
2026-05-23 20:29:27 +0200

    final final edits

--- src/App.tsx
+++ src/App.tsx
@@ -1859,10 +1859,10 @@ useEffect(() => {
     opacity: 0.86,
   }}
 >
-  Oliver McGarvey is a Canadian film director working between Edmonton and Berlin across documentary, narrative and commissioned films.
+  Oliver McGarvey is a Canadian film director working between Edmonton and Berlin, spanning documentary, narrative and commissioned films.
   <br />
   <br />
-  His work can be described as a kind of earnest searching. Travelling beneath the surface into the realm of our beautiful, but distorted inner experiences. Open in form, he allows each project to grow naturally, yet gravitates towards visual restraint and emotional realism.
+  His work can be described as a kind of earnest searching. Travelling beneath the surface into the realm of our beautiful, but distorted inner experiences. Open in form, he allows each project to grow naturally, yet gravitates towards visual restraint and poetic realism.
   <br />
   <br />
   Alongside being a regular Canada Council jury member, he is currently in post-production on a hybrid documentary and developing his debut feature screenplay with support from Telefilm Canada.
@@ -2190,11 +2190,11 @@ useEffect(() => {
       }}
     >
       <div style={{ marginBottom: 20 }}>
-        Oliver McGarvey is a Canadian film director working between Edmonton and Berlin across documentary, narrative and commissioned films.
+        Oliver McGarvey is a Canadian film director working between Edmonton and Berlin, spanning documentary, narrative and commissioned films.
       </div>
 
       <div style={{ marginBottom: 20 }}>
-        His work can be described as a kind of earnest searching. Travelling beneath the surface into the realm of our beautiful, but distorted inner experiences. Open in form, he allows each project to grow naturally, yet gravitates towards visual restraint and emotional realism.
+        His work can be described as a kind of earnest searching. Travelling beneath the surface into the realm of our beautiful, but distorted inner experiences. Open in form, he allows each project to grow naturally, yet gravitates towards visual restraint and poetic realism.
       </div>
 
       <div>
